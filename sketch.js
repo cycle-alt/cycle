c3c3336bd5b72a78bc10f4fa3f9c423500bc6da4
cycle-alt <81953576+cycle-alt@users.noreply.github.com>
2021-04-05 12:58:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cycles, cycle1, cycle2;
+
+
+
+function preload(){
+ 
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  if(playerCount === 2){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+  if(gameState === 2){
+    game.end();
+  }
+}
